7d67b24966b35a54842f304e381ea857333a28a3
Diegon01 <perezgonzalezdiego3@gmail.com>
2023-10-06 14:58:23 -0300

    nuevas vistas

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('crear_rubro');
+        return view('alta_proveedor');
     }
     public function rubro_created(): string
     {
---
+++ app/Views/alta_proveedor.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alta de Proveedor</title>
+    <!-- Agrega los estilos de Tailwind CSS -->
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+
+<body class="bg-gray-100 h-screen flex justify-center items-center">
+    <div class="bg-white p-8 rounded shadow-md max-w-md w-full text-center">
+        <h1 class="text-2xl font-semibold mb-6 text-center">Alta de Proveedor</h1>
+
+        <!-- Formulario para el alta de proveedor -->
+        <form action="/ruta/para/guardar/proveedor" method="POST">
+
+            <!-- Campo Nombre de Proveedor -->
+            <div class="mb-4 flex items-center">
+                <label for="nombreProveedor" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Nombre de Proveedor</label>
+                <input type="text" id="nombreProveedor" name="nombreProveedor" class="mt-1 p-2 w-full border rounded">
+            </div>
+
+            <!-- Campo Persona de Contacto -->
+            <div class="mb-4 flex items-center">
+                <label for="personaContacto" class="block text-sm font-medium text-gray-600 w-[150px] text-left ">Persona de Contacto</label>
+                <input type="text" id="personaContacto" name="personaContacto" class="mt-1 p-2 w-full border rounded">
+            </div>
+
+            <!-- Campo Número de Contacto -->
+            <div class="mb-4 flex items-center">
+                <label for="numeroContacto" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Número de Contacto</label>
+                <input type="text" id="numeroContacto" name="numeroContacto" class="mt-1 p-2 w-full border rounded">
+            </div>
+
+            <!-- Campo RUT -->
+            <div class="mb-4 flex items-center">
+                <label for="rut" class="block text-sm font-medium text-gray-600 w-[150px] text-left">RUT</label>
+                <input type="text" id="rut" name="rut" class="ml-2 mt-1 p-2 pr-5 w-full border rounded">
+            </div>
+
+            <!-- Campo Número de Cuenta -->
+            <div class="mb-4 flex items-center">
+                <label for="numeroCuenta" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Número de Cuenta</label>
+                <input type="text" id="numeroCuenta" name="numeroCuenta" class="mt-1 p-2 w-full border rounded">
+            </div>
+
+            <!-- Fecha de Vencimiento DGI -->
+            <div class="mb-4 flex items-center">
+                <label for="fechaVencimientoDgi" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Fecha de Vencimiento DGI</label>
+                <input type="date" id="fechaVencimientoDgi" name="fechaVencimientoDgi" class="mt-1 p-2 w-full border rounded">
+            </div>
+
+            <!-- Fecha de Vencimiento BPS -->
+            <div class="mb-4 flex items-center">
+                <label for="fechaVencimientoBps" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Fecha de Vencimiento BPS</label>
+                <input type="date" id="fechaVencimientoBps" name="fechaVencimientoBps" class="mt-1 p-2 w-full border rounded">
+            </div>
+
+            <div class="mt-8 space-x-2 flex justify-center items-center">
+                <!-- Campo RUPE -->
+                <div class="inline-flex items-center">
+                    <label for="rupe" class="block text-sm font-medium text-gray-600 mr-2">RUPE</label>
+                    <input type="checkbox" id="rupe" name="rupe" class="form-checkbox h-5 w-5 text-blue-600">
+                </div>
+                
+                <!-- Campo Empresa del Estado -->
+                <div class="inline-flex items-center">
+                    <label for="empresaEstado" class="block text-sm font-medium text-gray-600 mr-2">Empresa del Estado</label>
+                    <input type="checkbox" id="empresaEstado" name="empresaEstado" class="form-checkbox h-5 w-5 text-blue-600">
+                </div>
+            </div>
+
+            <!-- Botón de enviar -->
+            <div class="flex justify-center mt-8">
+                <button type="submit"
+                    class="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300">
+                    Añadir Provedor
+                </button>
+            </div>
+        </form>
+    </div>
+</body>
