710b64a731a0084e9233ab999b3bd8824f089880
SantiagoSosaM <santiago.sosa.m@estudiantes.utec.edu.uy>
2023-10-09 17:03:15 -0300

    asjfhksja

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('alta_proveedor');
+        return view('alta_ordenCompra');
     }
     public function rubro_created(): string
     {
