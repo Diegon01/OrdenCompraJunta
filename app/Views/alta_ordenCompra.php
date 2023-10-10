a4d6c7637d15c416b90afafb8ebd9f6e3929c781
Diegon01 <perezgonzalezdiego3@gmail.com>
2023-10-09 21:46:53 -0300

    orden de compra vista
    asdad

--- app/Views/alta_ordenCompra.php
+++ app/Views/alta_ordenCompra.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,36 +8,50 @@
     <!-- Incluye el archivo de estilo de Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
     <style>
-        /* Estilo personalizado para el contenedor de los inputs */
-        .inputs-container {
-            max-height: 290px; /* 6 filas * 64px (altura de una fila) */
-            overflow-y: auto;
-        }
+       /* Estilo personalizado para el contenedor de los inputs */
+.inputs-container {
+    max-height: 270px; /* 6 filas * 64px (altura de una fila) */
+    overflow-y: auto;
+    margin-top: 2rem; /* Ajusta el margen superior según sea necesario */
+    position: relative;
+}
+
+/* Estilo para los encabezados de la tabla */
+.inputs-container th {
+    position: sticky;
+    top: 0;
+    background-color: #f8f9fa; /* Color de fondo para los encabezados */
+    z-index: 1; /* Asegura que los encabezados estén por encima de la barra de desplazamiento */
+}
+
+textarea {
+    height: 200px!important; 
+    width: 400px!important;
+    resize: none; 
+}
     </style>
 </head>
+
 <body class="bg-gray-100 p-4 pt-8">
     <div class="max-w-6xl mx-auto bg-white p-4 rounded-md shadow-md">
         <div class="max-w-4xl mx-auto bg-gray-200 p-4 rounded-md shadow-md">
-            <h1 class="text-2xl font-semibold mb-4 text-center">Formulario de Productos</h1>
+            <h1 class="text-2xl font-semibold mb-4 text-center">Nueva Orden de Compra</h1>
             <!-- Formulario para ingresar datos de productos en forma de tabla -->
             <form action="procesar_formulario.php" method="POST">
-                <table class="w-full mb-4">
-                    <!-- Encabezados de la tabla -->
-                    <tr>
-                        <th class="pr-4 font-semibold text-center">Nombre Producto</th>
-                        <th class="pr-4 font-semibold text-center">Precio Estimado</th>
-                        <th class="pr-4 font-semibold text-center">Nombre Rubro</th>
-                        <th class="pr-4 font-semibold text-center">Código Rubro</th>
-                        <th class="pr-4 font-semibold text-center">Saldo Rubro</th>
-                        <th class="pr-4 font-semibold text-center">Acciones</th>
-                    </tr>
-                </table>
                 <!-- Contenedor con altura fija y desbordamiento solo para los inputs -->
                 <div class="inputs-container">
                     <table class="w-full">
+                        <!-- Encabezados de la tabla -->
+                        <tr>
+                            <th class="pr-4 font-semibold text-center">Nombre Producto</th>
+                            <th class="pr-4 font-semibold text-center">Precio Estimado</th>
+                            <th class="pr-4 font-semibold text-center">Nombre Rubro</th>
+                            <th class="pr-4 font-semibold text-center">Código Rubro</th>
+                            <th class="pr-4 font-semibold text-center">Saldo Rubro</th>
+                            <th class="pr-4 font-semibold text-center">Acciones</th>
+                        </tr>
                         <!-- Campos del producto aquí -->
                         <tr class="producto-clone">
-                            <!-- ... Inputs ... -->
                             <td class="text-center">
                                 <div class="input-wrapper">
                                     <input type="text" name="nombre" class="mt-1 p-2 w-full border rounded-md">
@@ -70,24 +85,43 @@
                 </div>
                 <!-- Botón de Enviar -->
                 <div class="mt-4 text-center">
-                    <button id="agregar-producto" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Agregar Producto</button>
+                    <button id="agregar-producto" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Agregar Producto</button>
                 </div>
-                <div class="mt-4 text-center">
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Guardar Producto</button>
-                </div>
+                
             </form>
+        </div>
+            
+        
+        <!-- Contenedor para las áreas de texto -->
+       
+    <div class="max-w-4xl mx-auto bg-gray-200 shadow-md p-4 mt-4 rounded-md flex justify-center items-center">
+        <div class="mr-10">
+            <label class="font-semibold block text-lg mb-2 text-center" for="texto1">Posibles Proveedores:</label>
+            <textarea id="texto1" class="border p-2 rounded w-full h-32 text-center self-center mx-auto" placeholder="Ingrese posibles proveedores" spellcheck="false"></textarea>
+        </div>
+        <div>
+            <label class="font-semibold block text-lg mb-2 text-center" for="texto2">Descripción:</label>
+            <textarea id="texto2" class="border p-2 rounded w-full h-32 text-center self-center mx-auto" placeholder="Ingrese descripción" spellcheck="false"></textarea>
         </div>
     </div>
 
+        
+        
+        <div class="mt-4 text-center">
+            <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Crear Orden de Compra</button>
+        </div>
+
+    </div>
+
     <script>
-        document.getElementById("agregar-producto").addEventListener("click", function(event) {
+        document.getElementById("agregar-producto").addEventListener("click", function (event) {
             event.preventDefault();
 
             // Clonar el primer conjunto de campos de producto
             var productoClonado = document.querySelector(".producto-clone").cloneNode(true);
 
             // Limpiar los valores de los campos clonados
-            productoClonado.querySelectorAll("input").forEach(function(input) {
+            productoClonado.querySelectorAll("input").forEach(function (input) {
                 input.value = "";
             });
 
@@ -95,7 +129,7 @@
             document.querySelector(".inputs-container table").appendChild(productoClonado);
 
             // Agrega el evento de clic para los botones de eliminar en el producto clonado
-            productoClonado.querySelector(".delete-producto").addEventListener("click", function(event) {
+            productoClonado.querySelector(".delete-producto").addEventListener("click", function (event) {
                 event.preventDefault();
 
                 // Encuentra el padre (fila) del botón y elimínala
@@ -105,4 +139,5 @@
         });
     </script>
 </body>
+
 </html>
