875fdb792116bb322cfffa72f91e153f3af279a8
SantiagoSosaM <santiago.sosa.m@estudiantes.utec.edu.uy>
2023-11-22 02:08:52 -0300

    a

--- app/Views/ingresar_ofertas.php
+++ app/Views/ingresar_ofertas.php
@@ -77,7 +77,7 @@
             ?>
             <?php if ($enlace['proveedor_id'] === $proveedor['id']) : ?>
                 <!-- Modal -->
-                <div id="modal<?= $proveedorId ?>" class="modal hidden fixed inset-0 flex items-center justify-center z-50">
+                <div id="modal<?= $proveedorId ?>" class="modal hidden fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
                     <div class="modal-content bg-white border border-gray-300 shadow-md rounded-lg p-6 w-3/4">
                         <button type="button" class="top-4 right-4 text-red-600 text-3xl" onclick="closeModal('modal<?= $proveedorId ?>')">&times;</button>
                         <div class="productos-container p-0">
@@ -152,29 +152,69 @@
 
 
                 <!-- Ver Modal -->
-                <div id="vermodal<?= $proveedorId ?>" class="modal hidden fixed inset-0 flex items-center justify-center z-50">
-                    <div class="modal-content bg-white border border-gray-300 shadow-md rounded-lg p-6 w-3/4">
-                        <button type="button" class="top-4 right-4 text-red-600 text-3xl" onclick="closeModal('vermodal<?= $proveedorId ?>')">&times;</button>
-                        <div class="productos-container p-0">
-                            <br>
-                            <label class="font-semibold text-2xl pb-2 block text-center">Detalles de <?= $proveedorNombre ?>:</label>
-                            Persona de contacto: <?= $proveedor['persona_de_contacto'] ?>
-                            <br><br>
-                            Número de contacto: <?= $proveedor['numero_de_contacto'] ?>
-                            <br><br>
-                            RUT: <?= $proveedor['RUT'] ?>
-                            <br><br>
-                            Número de cuenta: <?= $proveedor['numero_de_cuenta'] ?>
-                            <br><br>
-                            Fecha de vencimiento de DGI: <?= $proveedor['fecha_de_vencimiento_dgi'] ?>
-                            <br><br>
-                            Fecha de vencimiento de BPS: <?= $proveedor['fecha_de_vencimiento_bps'] ?>
-                            <br><br>
-                            <div id="modalContent<?= $proveedorId ?>"></div>
+                <div id="vermodal<?= $proveedorId ?>" class="modal hidden fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+                    <div class="modal-content bg-white p-4 rounded shadow-lg w-1/4 mx-auto my-16">
+                    <button type="button" class="top-4 right-4 text-red-600 text-3xl" onclick="closeModal('vermodal<?= $proveedorId ?>')">&times;</button>
+                    <h1 class="text-2xl font-semibold mb-2 text-center">Detalles de <?= $proveedorNombre ?>:</h1>
+        
+                    <div class="mb-4 flex items-center">
+                        <input type="text" id="idProveedor" name="idProveedor" class="mt-1 p-2 w-full border rounded" style="display: none;" readonly>
+                    </div>
+
+                    <div class="mb-4 flex items-center">
+                        <label for="personaContacto" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Persona de Contacto</label>
+                        <input type="text" id="personaContacto" name="personaContacto" class="mt-1 p-2 w-full border rounded" value="<?= $proveedor['persona_de_contacto'] ?>" readonly>
+                    </div>
+
+                    <div class="mb-4 flex items-center">
+                        <label for="numeroContacto" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Número de Contacto</label>
+                        <input type="text" id="numeroContacto" name="numeroContacto" class="mt-1 p-2 w-full border rounded" value="<?= $proveedor['numero_de_contacto'] ?>" readonly>
+                    </div>
+
+                    <div class="mb-4 flex items-center">
+                        <label for="rut" class="block text-sm font-medium text-gray-600 w-[150px] text-left">RUT</label>
+                        <input type="text" id="rut" name="rut" class="ml-2 mt-1 p-2 pr-5 w-full border rounded" value="<?= $proveedor['RUT'] ?>" readonly>
+                    </div>
+
+                    <div class="mb-4 flex items-center">
+                        <label for="numeroCuenta" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Número de Cuenta</label>
+                        <input type="text" id="numeroCuenta" name="numeroCuenta" class="mt-1 p-2 w-full border rounded" value="<?= $proveedor['numero_de_cuenta'] ?>" readonly>
+                    </div> 
+
+                    <div class="mb-4 flex items-center">
+                        <label for="fechaVencimientoDgi" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Fecha de Vencimiento DGI</label>
+                        <input type="date" id="fechaVencimientoDgi" name="fechaVencimientoDgi" class="mt-1 p-2 w-full border rounded" value="<?= $proveedor['fecha_de_vencimiento_dgi'] ?>" readonly>
+                    </div>
+
+                    <div class="mb-4 flex items-center">
+                        <label for="fechaVencimientoBps" class="block text-sm font-medium text-gray-600 w-[150px] text-left">Fecha de Vencimiento BPS</label>
+                        <input type="date" id="fechaVencimientoBps" name="fechaVencimientoBps" class="mt-1 p-2 w-full border rounded" value="<?= $proveedor['fecha_de_vencimiento_bps'] ?>" readonly>
+                    </div>
+
+                    <div class="mt-8 space-x-2 flex justify-center items-center">
+                        <div class="inline-flex items-center">
+                            <label for="rupe" class="block text-sm font-medium text-gray-600 mr-2">RUPE</label>
+                            <?php if ($proveedor['rupe'] === '1') { ?>
+                                <input type="checkbox" id="rupe" name="rupe" class="form-checkbox h-5 w-5 text-blue-600" checked disabled>
+                            <?php } ?>
+                            <?php if ($proveedor['rupe'] === '0') { ?>
+                                <input type="checkbox" id="rupe" name="rupe" class="form-checkbox h-5 w-5 text-blue-600" disabled>
+                            <?php } ?>
+                        </div>
+
+                        <div class="inline-flex items-center">
+                            <label for="empresaEstado" class="block text-sm font-medium text-gray-600 mr-2">Empresa del Estado</label>
+                            <?php if ($proveedor['empresa_del_estado'] === '1') { ?>
+                                <input type="checkbox" id="empresaEstado" name="empresaEstado" class="form-checkbox h-5 w-5 text-blue-600" checked disabled>
+                            <?php } ?>
+                            <?php if ($proveedor['empresa_del_estado'] === '0') { ?>
+                                <input type="checkbox" id="empresaEstado" name="empresaEstado" class="form-checkbox h-5 w-5 text-blue-600" disabled>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
-                <?php endif; ?>
+            </div>
+            <?php endif; ?>
             <?php
             }}
             ?>
